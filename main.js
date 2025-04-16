d14e722e6428f1c0a28abe41775fadf04233efe3
Nika <nikalyalina239@gmail.com>
2025-04-16 07:32:54 +0500

    create var for game

--- main.js
+++ main.js
@@ -9,3 +9,19 @@ const scoreText = document.querySelector(".wrapper .score");
 const gameOverBox = document.querySelector(".wrapper .game-over ");
 const restartBtn = document.querySelector(".wrapper .game-over .restart-btn");
 const highScoreText = document.querySelector(".wrapper .high-score");
+
+let gameStart = false; //для повторного запуска игры
+let jump = true; // блокирует повторые прыжки
+let score = 0; //текущий счет
+let globalRunInterval, globalCactusInterval, globalScoreInterval; //для остановки интервалов
+
+let highScore = localStorage.getItem("high-score") || "00000";//Получает сохранённый рекорд из localStorage, если нет — устанавливает “00000”
+highScoreText.innerHTML = "HI " + highScore;
+
+let iniGme =()=>{ // проверяет началась игра или нет, если нет вызывает startGame 
+
+    if(!gameStart){
+        startGame();
+    }
+    gameStart = true;
+}
